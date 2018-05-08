8c03d02509aacacd1d3762afdc95f514248725a3
Austin Pierce <Austin.Pierce@echo.com>
2018-05-08 11:14:39 -0500

    2_DB01_LoadLink_Remove_From_Replication.sql

--- 2_DB01_LoadLink_Remove_From_Replication.sql
+++ 2_DB01_LoadLink_Remove_From_Replication.sql
@@ -6,11 +6,15 @@ Remove table from replication, will add back after schema changes
 
 Run in DB01VPRD Equivilant 
 */
-USE databaseName;
-GO
+--USE databaseName;
+--GO
 
-SET TRANSACTION ISOLATION LEVEL READ COMMITTED;  
-GO 
+--SET TRANSACTION ISOLATION LEVEL READ COMMITTED;  
+--GO 
 
-exec sys.sp_dropsubscription @publication = 'PublicationdatabaseName',@article = 'dbo.tableName', @subscriber = N'all',@destination_db = N'all'
-exec sp_droparticle @publication = 'PublicationdatabaseName', @article = 'dbo.tableName',@force_invalidate_snapshot = 0
+--exec sys.sp_dropsubscription @publication = 'PublicationdatabaseName',@article = 'dbo.tableName', @subscriber = N'all',@destination_db = N'all'
+--exec sp_droparticle @publication = 'PublicationdatabaseName', @article = 'dbo.tableName',@force_invalidate_snapshot = 0
+
+--==========================================================================================
+-- Tables not replicated
+--==========================================================================================
